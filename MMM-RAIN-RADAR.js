87705f66a26dc28cad7ad7bdf2f6efa9ed65ff39
vincep5 <vince.peri@gmail.com>
2019-05-24 14:07:39 -0500

    add ability to configure different settings for the radar map

--- MMM-RAIN-RADAR.js
+++ MMM-RAIN-RADAR.js
@@ -10,19 +10,31 @@
 Module.register("MMM-RAIN-RADAR",{
 
 	defaults: {
-			height:"270px",
-			width:"400px",
-            animationSpeed: "0",
-            updateInterval: 60 * 60 * 1000,
+		height: "270px",
+		width: "400px",
+		zoomLevel: 6,
+		mapType: 3, //0-Road Map 1-Satellite 2-Dark Map 3-OpenStreetMaps 4-Light Map
+		color: 3, //0-Original 1-Universal Blue 2-TITAN 3-The Weather Channel 5-NEXRAD Level-III 6-RAINBOW @ SELEX-SI
+		snow: 1,
+		smoothing: 1,
+		opacity: 88,
+		fastAnimation: 0,
+		coverage: 0,
+		darkTheme: 1,
+		UTCtime: 0,
+		legend: 1,
+		legendMin: 0, //set legend to 1 if you want legendMin to show
+		animate: 1,
+		updateInterval: 60 * 60 * 1000,
 	},
 
-    start: function () {
+	start: function () {
 		self = this;
 	},
 
 	getStyles: function() {
-        return ["MMM-RAIN-RADAR.css"];
-    },
+		return ["MMM-RAIN-RADAR.css"];
+	},
 	
 	getDom: function() {
 
@@ -32,15 +44,13 @@ Module.register("MMM-RAIN-RADAR",{
 		iframe.style = "border:0;height:50vh";
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
-		type="text/javascript";
 		
 		let lat = this.config.lat;
 		let lon = this.config.lon;
-		let legend = this.config.legend;
 		
 		// tail for src
-		let tail = (legend === undefined || legend) ? "6&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=1&rmt=3&c=3&o=88&lm=0&th=1&sm=1&sn=1" : "6&oFa=0&oC=0&oU=0&oCS=0&oF=0&oAP=1&rmt=3&c=3&o=88&lm=0&th=1&sm=1&sn=1";
-		iframe.src=`https://www.rainviewer.com/map.html?loc=${lat},${lon},${tail}`;
+		let tail = `${this.config.zoomLevel}&oFa=${this.config.fastAnimation}&oC=${this.config.coverage}&oU=${this.config.UTCtime}&oCS=${this.config.legend}&oF=0&oAP=${this.config.animate}&rmt=${this.config.mapType}&c=${this.config.color}&o=${this.config.opacity}&lm=${this.config.legendMin}&th=${this.config.darkTheme}&sm=${this.config.smoothing}&sn=${this.config.snow}`;
+		iframe.src = `https://www.rainviewer.com/map.html?loc=${lat},${lon},${tail}`;
 
 		return iframe;
 	},
